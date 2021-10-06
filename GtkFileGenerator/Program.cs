a273f003ed41ec4b59555fada11b0d7cf2329a08
gtk <work.eggpop@gmail.com>
2021-10-06 21:33:17 +0900

    update

--- GtkFileGenerator/Program.cs
+++ GtkFileGenerator/Program.cs
@@ -41,28 +41,25 @@ namespace GtkFileGenerator
                     Console.WriteLine("Class name is missing. Copy and paste the class name");
                     return;
                 }
-
-                if (className != "")
-                {
-                    string classStr = clsFile._load_static( clsFile._getExePath_replace("classTemplate.txt"));
-                    string gladeStr = clsFile._load_static( clsFile._getExePath_replace("gladeTemplate.txt"));
-
-                    classStr = classStr._replaceReplaceStr("{$ProjectName}", clsArgsConfig.Instance().ProjectName);
-                    classStr = classStr._replaceReplaceStr("{$className}",className);
                 
-                    gladeStr = gladeStr._replaceReplaceStr("{$ProjectName}", clsArgsConfig.Instance().ProjectName);
-                    gladeStr = gladeStr._replaceReplaceStr("{$className}", className);
+                string classStr = clsFile._load_static( clsFile._getExePath_replace("classTemplate.txt"));
+                string gladeStr = clsFile._load_static( clsFile._getExePath_replace("gladeTemplate.txt"));
 
-                    string classFilePath = clsArgsConfig.Instance().ProjectFolder + "/" + className + ".cs";
-                    string gladeFilePath = clsArgsConfig.Instance().ProjectFolder + "/" + className + ".glade";
+                classStr = classStr._replaceReplaceStr("{$ProjectName}", clsArgsConfig.Instance().ProjectName);
+                classStr = classStr._replaceReplaceStr("{$className}",className);
+            
+                gladeStr = gladeStr._replaceReplaceStr("{$ProjectName}", clsArgsConfig.Instance().ProjectName);
+                gladeStr = gladeStr._replaceReplaceStr("{$className}", className);
 
-                    clsFile._saveFilePath(classStr,classFilePath);
-                    clsFile._saveFilePath(gladeStr,gladeFilePath);
-                    
-                    Console.WriteLine("I wrote it out " + classFilePath);
-                    Console.WriteLine("I wrote it out " + gladeFilePath);
-                    
-                }
+                string classFilePath = clsArgsConfig.Instance().ProjectFolder + "/" + className + ".cs";
+                string gladeFilePath = clsArgsConfig.Instance().ProjectFolder + "/" + className + ".glade";
+
+                clsFile._saveFilePath(classStr,classFilePath);
+                clsFile._saveFilePath(gladeStr,gladeFilePath);
+                
+                Console.WriteLine("I wrote it out " + classFilePath);
+                Console.WriteLine("I wrote it out " + gladeFilePath);
+                
             }
             catch (Exception e)
             {
